317fb03b5c5b98dbac5ea7f51f51990fee5afaf2
Fwalt237 <patrickkamwa17@gmail.com>
2025-03-19 09:21:35 +0100

    Splitted theLine

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -14,7 +14,8 @@ public class FileReader {
         try(BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file))){
             String line = bufferedReader.readLine();
             while (line != null) {
-                lines.add(line);
+                String[] split = line.split(": ");
+                lines.add(split[1]);
                 line = bufferedReader.readLine();
             }
             return new Profile(lines.get(0), Integer.valueOf(lines.get(1)), lines.get(2), Long.valueOf(lines.get(3)));
