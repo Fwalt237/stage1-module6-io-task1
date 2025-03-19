a02895f7feab77dc3fe4190f7b809bd2880cde3d
Fwalt237 <patrickkamwa17@gmail.com>
2025-03-19 09:14:39 +0100

    Modified FileReader

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,29 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+
+    public Profile getDataFromFile(File file)  {
+        List<String> lines = new ArrayList<>();
+        try(BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file))){
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                lines.add(line);
+                line = bufferedReader.readLine();
+            }
+            return new Profile(lines.get(0), Integer.valueOf(lines.get(1)), lines.get(2), Long.valueOf(lines.get(3)));
+        }catch (FileNotFoundException fnfe){
+            fnfe.printStackTrace();
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+        return null;
     }
 }
+
